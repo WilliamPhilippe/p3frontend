726fda38d4e5d99118db41c9bdb1ae572bf77d80
William Philippe <wplb@ic.ufal.br>
2021-08-15 09:46:08 -0300

    build

---
+++ build/precache-manifest.589bbc8ad863cc39474f513bbe35d4cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f050b08537721a5ed851956870ae56e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a44c88ab497031159512",
+    "url": "/static/css/2.a8f673c4.chunk.css"
+  },
+  {
+    "revision": "a44c88ab497031159512",
+    "url": "/static/js/2.a9e95465.chunk.js"
+  },
+  {
+    "revision": "4084b622f3960e1b2d37",
+    "url": "/static/js/main.12030ed8.chunk.js"
+  },
+  {
+    "revision": "13d189a423a5fcb5309c",
+    "url": "/static/js/runtime-main.c69e9083.js"
+  }
+]);
